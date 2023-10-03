3e08494a9c2c2c5cf57cd4b7bca55c1e3e2dcb30
Ramzi Bani Saad <lastgme@gmail.com>
2023-10-03 19:27:05 +0000

    Pending changes exported from your codespace

--- script.js
+++ script.js
@@ -64,14 +64,18 @@ userInput.addEventListener("keypress", function(event) {
     };
 });
 
+let i = 1;
+
 helpSection.addEventListener("click", function() {
-    if (helpTextArrow.src === "https://cdn.lordicon.com/albqovim.json") {
-        helpTextArrow.src = "https://cdn.lordicon.com/xdakhdsq.json"
-    } else if (helpTextArrow.src === "https://cdn.lordicon.com/xdakhdsq.json") {
-        helpTextArrow.src = "https://cdn.lordicon.com/albqovim.json"
+    if (i == 1) {
+        helpTextArrow.style.transform = "rotate(180deg)";
+        i = 2;
+    } else if (i == 2) {
+        helpTextArrow.style.transform = "rotate(0deg)";
+        i--;
     };
     helpText.classList.toggle("hidden");
-    window.scrollBy(0, 386);
+    window.scrollBy(0, 800);
 });
 
 appointmentCategoryButton.addEventListener("click", function() {
@@ -84,4 +88,56 @@ taskCategoryButton.addEventListener("click", function() {
     taskCategoryButton.classList.add("categoryClicked");
     appointmentCategoryButton.classList.remove("categoryClicked");
     userInput.placeholder = "Neue Aufgabe...";
+});
+
+theme1.addEventListener("click", function() {
+    theme1.classList.add("categoryClicked");
+    theme2.classList.remove("categoryClicked");
+    theme3.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#F8EFBA";
+});
+theme2.addEventListener("click", function() {
+    theme2.classList.add("categoryClicked");
+    theme1.classList.remove("categoryClicked");
+    theme3.classList.remove("categoryClicked");
+    theme4.classList.remove("categoryClicked");
+    theme5.classList.remove("categoryClicked");
+    theme6.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#FEA47F";
+});
+theme3.addEventListener("click", function() {
+    theme3.classList.add("categoryClicked");
+    theme1.classList.remove("categoryClicked");
+    theme2.classList.remove("categoryClicked");
+    theme4.classList.remove("categoryClicked");
+    theme5.classList.remove("categoryClicked");
+    theme6.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#FD7272";
+});
+theme4.addEventListener("click", function() {
+    theme4.classList.add("categoryClicked");
+    theme1.classList.remove("categoryClicked");
+    theme2.classList.remove("categoryClicked");
+    theme3.classList.remove("categoryClicked");
+    theme5.classList.remove("categoryClicked");
+    theme6.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#9AECDB";
+});
+theme5.addEventListener("click", function() {
+    theme5.classList.add("categoryClicked");
+    theme1.classList.remove("categoryClicked");
+    theme2.classList.remove("categoryClicked");
+    theme3.classList.remove("categoryClicked");
+    theme4.classList.remove("categoryClicked");
+    theme6.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#D6A2E8";
+});
+theme6.addEventListener("click", function() {
+    theme6.classList.add("categoryClicked");
+    theme1.classList.remove("categoryClicked");
+    theme2.classList.remove("categoryClicked");
+    theme3.classList.remove("categoryClicked");
+    theme4.classList.remove("categoryClicked");
+    theme5.classList.remove("categoryClicked");
+    document.body.style.backgroundColor = "#6D214F";
 });
